7cb810d8f82971874e51cbd7badf0bd86f0dc316
Reinoptland <reinoptland@gmail.com>
2021-09-25 09:33:07 +0200

    Move request logic to SpacexService, simplify error handling

--- index.js
+++ index.js
@@ -1,69 +1,89 @@
 import "./style.css";
 
-async function showLaunches(retries) {
-  document.getElementById("root").innerHTML = "<h1>Loading</h1>";
+class SpacexService {
+  constructor(baseUrl, retries = 3) {
+    this.baseUrl = baseUrl;
+    this.retries = retries;
+  }
 
-  try {
-    const response = await fetch("https://api.spacexdata.com/v4/launches");
-    const data = await response.json();
-    const ONE_YEAR_IN_SECONDS = 2628000 * 12;
-    const TODAY_IN_SECONDS = new Date() / 1000;
-    const dateLimit = TODAY_IN_SECONDS - ONE_YEAR_IN_SECONDS;
-    const launchesThisYear = data
-      .filter((launch) => launch.date_unix > dateLimit)
-      .sort((a, b) => b.date_unix - a.date_unix);
-
-    document.getElementById("root").innerHTML = "";
-
-    launchesThisYear.forEach((launch) => {
-      if (launch.links.patch.large) {
-        const container = document.createElement("div");
-        container.classList.add("container");
-
-        const name = document.createElement("h2");
-        name.classList.add("mission-name");
-        name.innerText = launch.name;
-        container.appendChild(name);
-
-        function showDetails() {
-          const cards = document.getElementsByClassName("container");
-          for (const card of cards) {
-            card.style.backgroundColor = "";
-          }
-
-          document.getElementById("overlay").style.display = "block";
-          document.getElementById("details").innerText = launch.details;
-          this.parentElement.style.backgroundColor = "#3836ff";
-        }
-
-        const img = document.createElement("img");
-        img.src = launch.links.patch.large;
-        img.classList.add("mission-patch");
-        container.appendChild(img);
-
-        const button = document.createElement("button");
-        button.onclick = showDetails;
-        button.innerText = "Details";
-        button.classList.add("button--details");
-
-        if (!launch.details) {
-          button.disabled = true;
-        }
-
-        container.appendChild(button);
-
-        document.getElementById("root").appendChild(container);
+  async getLaunches(retries = this.retries) {
+    try {
+      const response = await fetch(`${this.baseUrl}/launches`);
+      const data = await response.json();
+      return [null, data];
+    } catch (error) {
+      if (retries > 0) {
+        this.getLaunches(retries - 1);
+      } else {
+        return [error, null];
       }
-    });
-  } catch (error) {
-    console.log("Uh oh", error.message);
-    if (retries > 0) {
-      showLaunches(retries - 1);
-    } else {
-      document.getElementById("root").innerHTML =
-        "<h1>Sorry, Elon is busy, try again later</h1>";
     }
   }
+}
+
+const spacexService = new SpacexService("https://api.spacexdata.com/v4");
+
+async function showLaunches() {
+  document.getElementById("root").innerHTML = "<h1>Loading</h1>";
+
+  const [error, data] = await spacexService.getLaunches();
+
+  if (error) {
+    console.log("Uh oh", error.message);
+    document.getElementById("root").innerHTML =
+      "<h1>Sorry, Elon is busy, try again later</h1>";
+    return;
+  }
+
+  const ONE_YEAR_IN_SECONDS = 2628000 * 12;
+  const TODAY_IN_SECONDS = new Date() / 1000;
+  const dateLimit = TODAY_IN_SECONDS - ONE_YEAR_IN_SECONDS;
+  const launchesThisYear = data
+    .filter((launch) => launch.date_unix > dateLimit)
+    .sort((a, b) => b.date_unix - a.date_unix);
+
+  document.getElementById("root").innerHTML = "";
+
+  launchesThisYear.forEach((launch) => {
+    if (launch.links.patch.large) {
+      const container = document.createElement("div");
+      container.classList.add("container");
+
+      const name = document.createElement("h2");
+      name.classList.add("mission-name");
+      name.innerText = launch.name;
+      container.appendChild(name);
+
+      function showDetails() {
+        const cards = document.getElementsByClassName("container");
+        for (const card of cards) {
+          card.style.backgroundColor = "";
+        }
+
+        document.getElementById("overlay").style.display = "block";
+        document.getElementById("details").innerText = launch.details;
+        this.parentElement.style.backgroundColor = "#3836ff";
+      }
+
+      const img = document.createElement("img");
+      img.src = launch.links.patch.large;
+      img.classList.add("mission-patch");
+      container.appendChild(img);
+
+      const button = document.createElement("button");
+      button.onclick = showDetails;
+      button.innerText = "Details";
+      button.classList.add("button--details");
+
+      if (!launch.details) {
+        button.disabled = true;
+      }
+
+      container.appendChild(button);
+
+      document.getElementById("root").appendChild(container);
+    }
+  });
 }
 
 function closeDetails() {
@@ -73,5 +93,5 @@ function closeDetails() {
   }
 }
 
-showLaunches(3);
+showLaunches();
 document.querySelector(".button--close").onclick = closeDetails;
