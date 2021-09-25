7058c442ebecd0f9aa2701d3c9b154be1faffe0c
Reinoptland <reinoptland@gmail.com>
2021-09-25 10:20:29 +0200

    Split up remaining code in showLaunches into small functions

--- index.js
+++ index.js
@@ -34,51 +34,67 @@ function selectLaunchesFromLastYearWithPatches(launches) {
     .sort((a, b) => b.date_unix - a.date_unix);
 }
 
-async function showLaunches() {
+function displayLoading() {
   document.getElementById("root").innerHTML = "<h1>Loading</h1>";
+}
 
-  const [error, launches] = await spacexService.getLaunches();
-
-  if (error) {
-    console.log("Uh oh", error.message);
-    document.getElementById("root").innerHTML =
-      "<h1>Sorry, Elon is busy, try again later</h1>";
-    return;
-  }
-
+function clearPage() {
   document.getElementById("root").innerHTML = "";
+}
 
-  const launchesThisYear = selectLaunchesFromLastYearWithPatches(launches);
+function displayError(error) {
+  console.log("Uh oh", error.message);
+  document.getElementById("root").innerHTML =
+    "<h1>Sorry, Elon is busy, try again later</h1>";
+}
 
-  launchesThisYear.forEach((launch) => {
-    const template = document.getElementById("mission-card-template");
+function displayCards(launches) {
+  launches.forEach((launch) => {
     const root = document.getElementById("root");
-
-    const missionCard = template.content.cloneNode(true);
-
-    console.log(missionCard);
-
-    function showDetails() {
-      const cards = document.getElementsByClassName("container");
-      for (const card of cards) {
-        card.style.backgroundColor = "";
-      }
-
-      document.getElementById("overlay").style.display = "block";
-      document.getElementById("details").innerText = launch.details;
-      this.parentElement.style.backgroundColor = "#3836ff";
-    }
-
-    missionCard.querySelector(".mission-name").innerText = launch.name;
-    missionCard.querySelector(".mission-patch").src = launch.links.patch.large;
-    const detailsButton = missionCard.querySelector(".button--details");
-    detailsButton.onclick = showDetails;
-    if (!launch.details) {
-      detailsButton.disabled = true;
-    }
+    const missionCard = createMissionCard(launch);
 
     root.appendChild(missionCard);
   });
+}
+
+async function showLaunches() {
+  displayLoading();
+
+  const [error, launches] = await spacexService.getLaunches();
+
+  if (error) return displayError(error);
+
+  clearPage();
+
+  const launchesThisYear = selectLaunchesFromLastYearWithPatches(launches);
+
+  displayCards(launchesThisYear);
+}
+
+function createMissionCard(launch) {
+  const template = document.getElementById("mission-card-template");
+
+  const missionCard = template.content.cloneNode(true);
+
+  function showDetails() {
+    const cards = document.getElementsByClassName("container");
+    for (const card of cards) {
+      card.style.backgroundColor = "";
+    }
+
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("details").innerText = launch.details;
+    this.parentElement.style.backgroundColor = "#3836ff";
+  }
+
+  missionCard.querySelector(".mission-name").innerText = launch.name;
+  missionCard.querySelector(".mission-patch").src = launch.links.patch.large;
+  const detailsButton = missionCard.querySelector(".button--details");
+  detailsButton.onclick = showDetails;
+  if (!launch.details) {
+    detailsButton.disabled = true;
+  }
+  return missionCard;
 }
 
 function closeDetails() {
